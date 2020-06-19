92cb28bc22938b0d41253fc753b74ef30746b6ab
aldekatg <tagay.aldiyar@gmail.com>
2020-06-19 15:01:52 +0600

    Настройка кнопки сохранения.

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -11,229 +11,233 @@ import * as moment from "moment";
 import {DialogOverviewComponent} from "../dialog-overview/dialog-overview.component";
 
 export interface DialogData {
-    name: string;
-    phone: string;
-    datePerson: string;
-    status?: string;
+  name: string;
+  phone: string;
+  datePerson: string;
+  status?: string;
 }
 
 const STATUS_DATA = [
-    {value: 'call-0', viewValue: 'Связаться'},
-    {value: 'sink-1', viewValue: 'Думает'},
-    {value: 'pay-2', viewValue: 'Жду оплату'},
-    {value: 'payed-2', viewValue: 'Оплачено'},
-    {value: 'cancel-2', viewValue: 'Отказ'}
+  {value: 'call-0', viewValue: 'Связаться'},
+  {value: 'sink-1', viewValue: 'Думает'},
+  {value: 'pay-2', viewValue: 'Жду оплату'},
+  {value: 'payed-2', viewValue: 'Оплачено'},
+  {value: 'cancel-2', viewValue: 'Отказ'}
 ]
 
 @Component({
-    selector: 'app-home',
-    templateUrl: './home.component.html',
-    styleUrls: ['./home.component.css']
+  selector: 'app-home',
+  templateUrl: './home.component.html',
+  styleUrls: ['./home.component.css']
 })
 export class HomeComponent implements OnInit {
 
-    dataStorage: MatTableDataSource<any>;
-    dataStorageDead: MatTableDataSource<any>;
-    table: string[] = ['date', 'deadLine', 'name', 'phone', 'status', 'btn'];
-    tableData: string[] = ['date', 'dateStart', 'name', 'phone', 'status'];
-    data = [];
-    dataDeadLine = [];
-    disabled: boolean = false;
-    spinner = true;
-    form: FormGroup;
-    status = STATUS_DATA;
-    @ViewChild(MatSort, {static: true}) sort: MatSort;
+  dataStorage: MatTableDataSource<any>;
+  dataStorageDead: MatTableDataSource<any>;
+  table: string[] = ['date', 'deadLine', 'name', 'phone', 'status', 'btn'];
+  tableData: string[] = ['date', 'dateStart', 'name', 'phone', 'status'];
+  data = [];
+  dataDeadLine = [];
+  disabled: boolean = false;
+  spinner = true;
+  form: FormGroup;
+  status = STATUS_DATA;
+  @ViewChild(MatSort, {static: true}) sort: MatSort;
 
-    constructor(private _http: HttpClient,
-                private tasksService: TaskService,
-                public dialog: MatDialog,
-                public dateService: DateService,
-                private _snackBar: MatSnackBar) {
+  constructor(private _http: HttpClient,
+              private tasksService: TaskService,
+              public dialog: MatDialog,
+              public dateService: DateService,
+              private _snackBar: MatSnackBar) {
 
-    }
+  }
 
-    ngOnInit(): void {
-        this.dataStorage = new MatTableDataSource<any>()
-        this.dataStorageDead = new MatTableDataSource<any>()
-        this.form = new FormGroup({
-            dateStart: new FormControl('', Validators.required),
-            name: new FormControl('', Validators.required),
-            phone: new FormControl('', Validators.required),
-            status: new FormControl
-        })
-        this.loadData();
-    }
+  ngOnInit(): void {
+    this.dataStorage = new MatTableDataSource<any>()
+    this.dataStorageDead = new MatTableDataSource<any>()
+    this.form = new FormGroup({
+      dateStart: new FormControl('', Validators.required),
+      name: new FormControl('', Validators.required),
+      phone: new FormControl('', Validators.required),
+      status: new FormControl
+    })
+    this.loadData();
+  }
 
-    loadData() {
-        this.data = [];
-        this.dataDeadLine = [];
+  loadData() {
+    this.data = [];
+    this.dataDeadLine = [];
 
 // загрузка данных из firebase
-        this.tasksService.load().subscribe(value => {
-            let data = Object.keys(value);
-            for (let i = 0; i < data.length; i++) {
-                let info = Object.keys(value[data[i]])
-                for (let z = 0; z < info.length; z++) {
-                    value[data[i]][info[z]]['id'] = info[z];
-                    this.changeClick(value[data[i]][info[z]]);
-                    value[data[i]][info[z]]['deadLine'] = this.deadLine(value[data[i]][info[z]].dateStart, value[data[i]][info[z]].datePerson)
-                    this.data.push(value[data[i]][info[z]]);
-                    if (value[data[i]][info[z]].deadLine < 4)
-                        this.dataDeadLine.push(value[data[i]][info[z]])
-                    if (value[data[i]][info[z]].status == 'Отказ') {
-                        this.dataDeadLine.splice(this.dataDeadLine.indexOf(value[data[i]][info[z]]), 1);
-                    }
-                }
-
-
-            }
-            this.spinner = false;
-            this.dataStorage.data = this.data;
-            this.dataStorageDead.data = this.dataDeadLine;
-            this.dataStorageDead.sort = this.sort;
-        }, error => console.log(error))
-
-    }
-
-
-    changeClick(item) {
-        if (item.status == 'Отказ' || item.status == 'Оплачено') {
-            item['disabled'] = false;
-        } else {
-            item['disabled'] = true;
+    this.tasksService.load().subscribe(value => {
+      let data = Object.keys(value);
+      for (let i = 0; i < data.length; i++) {
+        let info = Object.keys(value[data[i]])
+        for (let z = 0; z < info.length; z++) {
+          value[data[i]][info[z]]['id'] = info[z];
+          this.changeColor(value[data[i]][info[z]]);
+          value[data[i]][info[z]]['deadLine'] = this.deadLine(value[data[i]][info[z]].dateStart, value[data[i]][info[z]].datePerson)
+          this.data.push(value[data[i]][info[z]]);
+          if (value[data[i]][info[z]].deadLine < 4)
+            this.dataDeadLine.push(value[data[i]][info[z]])
+          if (value[data[i]][info[z]].status == 'Отказ') {
+            this.dataDeadLine.splice(this.dataDeadLine.indexOf(value[data[i]][info[z]]), 1);
+          }
         }
-        switch (item.status) {
-            case 'Оплачено': {
-                item['color'] = '#009933';
-                break;
-            }
-            case 'Связаться': {
-                item['color'] = '#9933CC';
-                break;
-            }
-            case 'Думает': {
-                item['color'] = '#6666FF';
-                break;
-            }
-            case 'Отказ': {
-                item['color'] = '#FF3333';
-                break;
-            }
-            case 'Жду оплату': {
-                item['color'] = '#FF6633';
-                break;
-            }
-            default: {
-                item['color'] = 'white';
-                break;
-            }
-        }
+
+
+      }
+      this.spinner = false;
+      this.dataStorage.data = this.data;
+      this.dataStorageDead.data = this.dataDeadLine;
+      this.dataStorageDead.sort = this.sort;
+    }, error => console.log(error))
+
+  }
+
+  changeClick(item) {
+    this.changeColor(item);
+    if (item.status == 'Отказ' || item.status == 'Оплачено') {
+      item['disabled'] = false;
+    } else {
+      item['disabled'] = true;
+      this.updateItem(item)
     }
+  }
 
-    saveBtn(item) {
-        if (item.status == 'Оплачено') {
-            item.datePerson = this.payedFunc(item.datePerson);
-            console.log(item.datePerson)
-            this.deadLine(item.dateStart, item.datePerson);
-        } else if (item.status == 'Отказ') {
-            console.log('ОТКАЗ');
-        }
-        this.updateItem(item);
+  changeColor(item) {
+    switch (item.status) {
+      case 'Оплачено': {
+        item['color'] = '#009933';
+        break;
+      }
+      case 'Связаться': {
+        item['color'] = '#9933CC';
+        break;
+      }
+      case 'Думает': {
+        item['color'] = '#6666FF';
+        break;
+      }
+      case 'Отказ': {
+        item['color'] = '#FF3333';
+        break;
+      }
+      case 'Жду оплату': {
+        item['color'] = '#FF6633';
+        break;
+      }
+      default: {
+        item['color'] = 'white';
+        break;
+      }
     }
+  }
 
-    updateItem(item) {
-        this.tasksService.change(item).subscribe(value => {
-            console.log(value);
-            this.openSnackBar(item.status, 'status');
-            this.loadData();
-        }, error => {
-            console.log(error);
-        })
+  saveBtn(item) {
+    if (item.status == 'Оплачено') {
+      item.datePerson = this.payedFunc(item.datePerson);
+      console.log(item.datePerson)
+      this.deadLine(item.dateStart, item.datePerson);
+    } else if (item.status == 'Отказ') {
+      console.log('ОТКАЗ');
     }
+    this.updateItem(item);
+  }
 
-    openSnackBar(value, when) {
-        if (when == 'status') {
-            this._snackBar.open('Статус изменен на: ' + value, 'Закрыть', {
-                duration: 2000,
-                horizontalPosition: 'right',
-                verticalPosition: 'top',
-            });
-        } else if (when == 'create') {
-            this._snackBar.open('Новый пользователь ' + value + ' добавлен.', 'Закрыть', {
-                duration: 3000,
-                horizontalPosition: 'right',
-                verticalPosition: 'top',
-            });
+  updateItem(item) {
+    this.tasksService.change(item).subscribe(value => {
+      console.log(value);
+      this.openSnackBar(item.status, 'status');
+      this.loadData();
+    }, error => {
+      console.log(error);
+    })
+  }
 
-        }
+  openSnackBar(value, when) {
+    if (when == 'status') {
+      this._snackBar.open('Статус изменен на: ' + value, 'Закрыть', {
+        duration: 2000,
+        horizontalPosition: 'right',
+        verticalPosition: 'top',
+      });
+    } else if (when == 'create') {
+      this._snackBar.open('Новый пользователь ' + value + ' добавлен.', 'Закрыть', {
+        duration: 3000,
+        horizontalPosition: 'right',
+        verticalPosition: 'top',
+      });
+
     }
+  }
 
-    submit() {
-        let datePerson = this.form.value.dateStart;
-        let name = this.form.value.name;
-        let phone = this.form.value.phone;
-        let dateStart = this.form.value.dateStart;
-        let status = 'Связаться';
+  submit() {
+    let datePerson = this.form.value.dateStart;
+    let name = this.form.value.name;
+    let phone = this.form.value.phone;
+    let dateStart = this.form.value.dateStart;
+    let status = 'Связаться';
 
-        datePerson = this.dateFormatter(datePerson);
-        datePerson = this.payedFunc(datePerson)
-        dateStart = this.dateFormatter(dateStart);
+    datePerson = this.dateFormatter(datePerson);
+    datePerson = this.payedFunc(datePerson)
+    dateStart = this.dateFormatter(dateStart);
 
-        const task: Task = {
-            datePerson,
-            dateStart,
-            name,
-            phone,
-            status
-        }
-        this.tasksService.create(task).subscribe(task => {
-            this.data.push(task);
-            this.form.reset();
-            this.loadData();
-            this.openSnackBar(task.name, 'create');
-        }, error => console.log(error))
+    const task: Task = {
+      datePerson,
+      dateStart,
+      name,
+      phone,
+      status
     }
+    this.tasksService.create(task).subscribe(task => {
+      this.data.push(task);
+      this.form.reset();
+      this.loadData();
+      this.openSnackBar(task.name, 'create');
+    }, error => console.log(error))
+  }
 
-    payedFunc(item) { // Логика при статусе "Оплативший"
-        let date = moment(item, "DD-MM-YYYY");
-        let day = date.date();
-        let month = date.month() + 2;
-        let year = date.year();
-        console.log(day + '-' + month + '-' + year);
-        return day + '-' + month + '-' + year;
-    }
+  payedFunc(item) { // Логика при статусе "Оплативший"
+    let date = moment(item, "DD-MM-YYYY");
+    let day = date.date();
+    let month = date.month() + 2;
+    let year = date.year();
+    console.log(day + '-' + month + '-' + year);
+    return day + '-' + month + '-' + year;
+  }
 
-    dateFormatter(dateGet) {
-        let date = new Date(dateGet);
-        let dd = date.getDate();
-        let mm = date.getMonth() + 1;
-        let yyyy = date.getFullYear();
-        return dd + "-" + mm + "-" + yyyy;
-    }
+  dateFormatter(dateGet) {
+    let date = new Date(dateGet);
+    let dd = date.getDate();
+    let mm = date.getMonth() + 1;
+    let yyyy = date.getFullYear();
+    return dd + "-" + mm + "-" + yyyy;
+  }
 
-    deadLine(dateStart, dateEnd) { // Функция считает разность между текущей датой и датой окончания абонемента
-        let currentDay = this.dateService.date.value.format('DD-MM-YYYY');
-        let a = moment(dateStart, "DD-MM-YYYY");
-        let b = moment(dateEnd, "DD-MM-YYYY");
-        let c = moment(currentDay, "DD-MM-YYYY");
+  deadLine(dateStart, dateEnd) { // Функция считает разность между текущей датой и датой окончания абонемента
+    let currentDay = this.dateService.date.value.format('DD-MM-YYYY');
+    let a = moment(dateStart, "DD-MM-YYYY");
+    let b = moment(dateEnd, "DD-MM-YYYY");
+    let c = moment(currentDay, "DD-MM-YYYY");
 
-        // let newDate = this.addMonth(dateStart, c.month() - b.month())
-        // let ss = moment(newDate, "DD-MM-YYYY");
+    // let newDate = this.addMonth(dateStart, c.month() - b.month())
+    // let ss = moment(newDate, "DD-MM-YYYY");
 
-        let days = b.diff(c, 'days');
-        return days;
-    }
+    let days = b.diff(c, 'days');
+    return days;
+  }
 
-    clickRow(data) {
-        // const dialogRef = this.dialog.open(DialogOverviewComponent, {
-        //   width: '600px',
-        //   data: data
-        // });
-        //
-        // dialogRef.afterClosed().subscribe(result => {
-        // this.tasksService.change(task).subscribe(task => {
-        //   console.log(task)
-        // }, error => console.log(error))
-        // });
-    }
+  clickRow(data) {
+    // const dialogRef = this.dialog.open(DialogOverviewComponent, {
+    //   width: '600px',
+    //   data: data
+    // });
+    //
+    // dialogRef.afterClosed().subscribe(result => {
+    // this.tasksService.change(task).subscribe(task => {
+    //   console.log(task)
+    // }, error => console.log(error))
+    // });
+  }
 }
